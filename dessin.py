faf6d9f737c22725a16b54e6cc3b0924fc826d28
lobthere <lolool.le.vrai@gmail.com>
2022-09-09 09:09:42 -0400

    une fonction cercle
    on a def une fonction cercle qui contient la valeur min, la valeur max et le pas

--- dessin.py
+++ dessin.py
@@ -1,14 +1,8 @@
 import turtle # module pour le dessin
 
-a = 1
-while True:
-    if a <= 10:
-        turtle.pencolor("blue")
-        turtle.circle(a)
-        a += 1
-    elif a == 20:
-        break
-    else:
-        turtle.pencolor("red")
-        turtle.circle(a)
-        a += 1
+def cercle(r_min: int, r_max: int, pas: int):
+    for r_max in range(pas):
+        turtle.circle(r_min)
+        r_min += pas
+
+cercle(5, 50, 5)
