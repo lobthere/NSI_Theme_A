844c855d77754b7080afcedae357b7ed7f1ac587
lobthere <lolool.le.vrai@gmail.com>
2022-09-09 08:48:18 -0400

    changement couleur
    on change la couleur tout les 10 jusqu a 30

--- dessin.py
+++ dessin.py
@@ -1,7 +1,14 @@
 import turtle # module pour le dessin
 
-a = 10
-
-for i in range(9):
-    turtle.circle(a)
-    a += 5
+a = 1
+while True:
+    if a <= 10:
+        turtle.pencolor("blue")
+        turtle.circle(a)
+        a += 1
+    elif a == 20:
+        break
+    else:
+        turtle.pencolor("red")
+        turtle.circle(a)
+        a += 1
