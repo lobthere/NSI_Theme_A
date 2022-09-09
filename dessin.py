c9c9b8196f7c400348fa7976ebd7c13643e24518
lobthere <lolool.le.vrai@gmail.com>
2022-09-09 09:25:38 -0400

    demande
    on demande a l utilisateur le min , le max , et le pas

--- dessin.py
+++ dessin.py
@@ -1,8 +1,14 @@
 import turtle # module pour le dessin
 
 def cercle(r_min: int, r_max: int, pas: int):
-    for r_max in range(pas):
+    """
+    cercle(your_minimum_value, your_maximum_value, the_increment)
+    """
+    for r_max in range(r_max):
         turtle.circle(r_min)
         r_min += pas
 
-cercle(5, 50, 5)
+a = int(input("entre ta valeur min : "))
+b = int(input("entre ta valeur max : "))
+c = int(input("entre ton pas : "))
+cercle(a, b, c)
